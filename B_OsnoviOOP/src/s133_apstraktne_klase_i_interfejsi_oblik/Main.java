0bb6dd7ac45905f06fb0a96ff10b52d6c8c15cb2
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2022-10-29 09:12:12 +0200

    Pregledane i doterane vezbe s133 i s145.

--- B_OsnoviOOP/src/s133_apstraktne_klase_i_interfejsi_oblik/Main.java
+++ B_OsnoviOOP/src/s133_apstraktne_klase_i_interfejsi_oblik/Main.java
@@ -4,18 +4,6 @@ import alati.RADE;
 
 public class Main {
 
-	/*
-	 * Realizovati klasu Oblik sa atributima boja (String), tip (String), brojStrana
-	 * (int), standardnim konstruktorom i konstruktorom koji postavlja inicijalne
-	 * vrednosti, kao i metodama za promenu boje oblika i štampanje podataka o
-	 * obliku. • Realizovati klase Kvadrat (dodatni atribut duzinaStranice tipa
-	 * double) i Krug (dodatni atribut poluprecnik tipa double) koje nasleðuju klasu
-	 * Oblik i implementiraju metode za raèunanje površine i obima.
-	 * 
-	 * • Klase testirati kreiranjem više objekata u glavnom programu i pozivanjem metoda za
-	 * raèunanje površine i obima i štampu podataka.
-	 */
-	
 	public static void main(String[] args) {
 		
 		Kvadrat kvadrat = new Kvadrat(RADE.generisiBoju(), RADE.mrRobot(1.0, 21));
---
+++ B_OsnoviOOP/src/s999_polaganje/Main.java
@@ -0,0 +1,10 @@
+package s999_polaganje;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
